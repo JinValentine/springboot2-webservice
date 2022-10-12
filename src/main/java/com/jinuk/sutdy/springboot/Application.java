1de3e8ed77af23dce4c0db490cd6e869d1193185
JINUK JEONG <wlsdnr214@gmail.com>
2022-10-13 06:15:30 +0900

    deploy test

--- src/main/java/com/jinuk/sutdy/springboot/Application.java
+++ src/main/java/com/jinuk/sutdy/springboot/Application.java
@@ -2,6 +2,7 @@ package com.jinuk.sutdy.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 /*
   스프링 부트의 자동설정, 스프링 Bean 일기와 생성을 모두 자동으로 설정
@@ -10,7 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        /* run으로 내장 WAS를 실행한다. */
-        SpringApplication.run(Application.class, args);
+        /* run으로 내장 WAS를 실행한다. 스프링 부트 PID 설정 */
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
+
     }
 }
